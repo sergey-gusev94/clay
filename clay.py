bdc8ab8e1cb1e73edc39fbb9333266f97559640f
Sergey Gusev <94sergey.gusev@gmail.com>
2025-07-07 14:17:58 -0400

    fixed folder location

--- clay.py
+++ clay.py
@@ -197,7 +197,7 @@ def save_to_excel(
         The subsolver used, by default None
     """
     # Create data directory if it doesn't exist
-    data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
+    data_dir = os.path.join(os.path.dirname(__file__), 'data')
     os.makedirs(data_dir, exist_ok=True)
     
     # Path to Excel file
